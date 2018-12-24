5831a818dda11f3a18332f0178dfcdc35dc26837
Sen Li <seli@e1z1r7p16.42.us.org>
2018-12-24 13:58:03 -0800

    add braching command

---
+++ 08/src/CommandType.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"bufio"
+	"strings"
+)
+
+// CommandType map line string to hack language type
+type CommandType int
+
+const (
+	//Comment Comment or Empty string
+	Comment CommandType = iota
+	//Unknown undefined command
+	Unknown
+	//Add +
+	Add
+	//Sub -
+	Sub
+	//Neg * -1
+	Neg
+	//Eq ==
+	Eq
+	//Gt >
+	Gt
+	//Lt <
+	Lt
+	//And &
+	And
+	//Or |
+	Or
+	//Not !
+	Not
+	//Pop pop from stack to M
+	Pop
+	//Push push from M to stack
+	Push
+	//Label create a label for branching
+	Label
+	//IfGoto go to the label when last element on stack is true
+	IfGoto
+	//Goto got to the label unconditional
+	Goto
+)
+
+//getCommandType return the CommandType of the next line from scanner
+func getCommandType(scanner *bufio.Scanner) (CommandType, []string) {
+	line := strings.TrimSpace(scanner.Text())
+	firstPart := strings.Split(line, "//")[0]
+	valueblePart := strings.Split(firstPart, " ")
+	if len(valueblePart) <= 0 {
+		return Comment, valueblePart
+	} else if valueblePart[0] == "add" {
+		return Add, valueblePart
+	} else if valueblePart[0] == "sub" {
+		return Sub, valueblePart
+	} else if valueblePart[0] == "neg" {
+		return Neg, valueblePart
+	} else if valueblePart[0] == "eq" {
+		return Eq, valueblePart
+	} else if valueblePart[0] == "gt" {
+		return Gt, valueblePart
+	} else if valueblePart[0] == "lt" {
+		return Lt, valueblePart
+	} else if valueblePart[0] == "and" {
+		return And, valueblePart
+	} else if valueblePart[0] == "or" {
+		return Or, valueblePart
+	} else if valueblePart[0] == "not" {
+		return Not, valueblePart
+	} else if valueblePart[0] == "pop" {
+		return Pop, valueblePart
+	} else if valueblePart[0] == "push" {
+		return Push, valueblePart
+	} else if valueblePart[0] == "label" {
+		return Label, valueblePart
+	} else if valueblePart[0] == "if-goto" {
+		return IfGoto, valueblePart
+	} else if valueblePart[0] == "goto" {
+		return Goto, valueblePart
+	}
+	return Unknown, valueblePart
+}
---
+++ 08/src/VMTranslator.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func main() {
+	files := os.Args[1:]
+	for _, filePath := range files {
+		file, err := os.Open(filePath)
+		check(err)
+		asmFile, err := os.Create(strings.Replace(filePath, ".vm", ".asm", 1))
+		check(err)
+		defer file.Close()
+		defer asmFile.Close()
+
+		jumpCounter := 0
+		scanner := bufio.NewScanner(file)
+		scanner.Split(bufio.ScanLines)
+		writer := bufio.NewWriter(asmFile)
+		for scanner.Scan() {
+			command, instructions := getCommandType(scanner)
+			if Add <= command && command <= Not {
+				fmt.Fprint(writer, arithmeticTranslator(command, instructions, &jumpCounter))
+			} else if Pop <= command && command <= Push {
+				fmt.Fprint(writer, memoryAccessTranslator(command, instructions, &jumpCounter))
+			}
+		}
+		writer.Flush()
+	}
+}
